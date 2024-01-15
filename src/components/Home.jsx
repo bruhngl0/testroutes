a669856f088cbab45615fd57551a779a749e19cd
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-15 18:33:34 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const Home = () => {
+  return (
+   
+    <Link to = "/about">
+    <div>
+      home
+    </div>
+    </Link>
+  )
+}
+
+export default Home
